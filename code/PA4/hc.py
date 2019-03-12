f542f4b6f5d68f1584458b662549e4768f6b5728
geesimon <geesimon@hotmail.com>
2019-03-12 16:00:09 +0800

    Optimize perf to pass the test

--- code/PA4/hc.py
+++ code/PA4/hc.py
@@ -1,37 +1,49 @@
 import math
 
-def get_cluster_distrance(cluster1, cluster2, distance_method):
-    cluster1_size = len(cluster1)
-    cluster2_size = len(cluster2)
-    point_dim = len(cluster1[0])
-    all_distances = [0] * (cluster1_size * cluster2_size)
+# def get_cluster_distrance(cluster1, cluster2, similarity_method):
+#     cluster1_size = len(cluster1)
+#     cluster2_size = len(cluster2)
+#     point_dim = len(cluster1[0])
+#     all_distances = [0] * (cluster1_size * cluster2_size)
 
-    k = 0
-    for i in range(cluster1_size):
-        for j in range(cluster2_size):
-            #sub = [(x1 - x2) ** 2 for (x1, x2) in zip(cluster1[i], cluster2[j])]            
-            #all_distances[k] = math.sqrt(sum(sub))
-            sq = 0
-            for d in range(point_dim):
-                sq += (cluster1[i][d] - cluster2[j][d]) ** 2
-            all_distances[k] = math.sqrt(sq)
-            #all_distances[k] = sq
+#     k = 0
+#     for i in range(cluster1_size):
+#         for j in range(cluster2_size):
+#             #sub = [(x1 - x2) ** 2 for (x1, x2) in zip(cluster1[i], cluster2[j])]            
+#             #all_distances[k] = math.sqrt(sum(sub))
+#             sq = 0
+#             for d in range(point_dim):
+#                 sq += (cluster1[i][d] - cluster2[j][d]) ** 2
+#             all_distances[k] = math.sqrt(sq)
+#             #all_distances[k] = sq
 
-            k += 1
+#             k += 1
 
-    if distance_method == 0:
-        return min(all_distances)
-    elif distance_method == 1:
-            return max(all_distances)
-    elif distance_method == 2:
-        return sum(all_distances) / len(all_distances)
-    else:
-        raise Exception('Bad Distance Method!')
+#     if similarity_method == 0:
+#         return min(all_distances)
+#     elif similarity_method == 1:
+#             return max(all_distances)
+#     elif similarity_method == 2:
+#         return sum(all_distances) / len(all_distances)
+#     else:
+#         raise Exception('Bad Similarity Method!')
 
-def getAHC(points, K, distance_method):
+def getAHC(points, K, similarity_method):
     point_count = len(points)
+    point_dim = len(points[0])
     cluster_count = point_count
     clusters = [[i] for i in range(cluster_count)]
+    similarity_matrix = [[0] * point_count for i in range(point_count)]
+
+#   Calcuate similarity between each point
+    for i in range(point_count - 1):
+        for j in range(i + 1, point_count):
+            s = 0
+            for d in range(point_dim):
+                s += (points[i][d] - points[j][d]) ** 2
+            s = math.sqrt(s)
+            similarity_matrix[i][j] = s
+            similarity_matrix[j][i] = s
 
     while cluster_count > K:
         min_distance = 1e20 #math.inf
@@ -39,9 +51,25 @@ def getAHC(points, K, distance_method):
         min_y = 0
         for x in range(cluster_count - 1):
             for y in range((x + 1), cluster_count):
-                cluster1_points = [points[i] for i in clusters[x]]
-                cluster2_points = [points[i] for i in clusters[y]]
-                d = get_cluster_distrance(cluster1_points, cluster2_points, distance_method)
+                cluster_similarity = [0] * (len(clusters[x]) * len(clusters[y]))
+                i = 0
+                for c1 in clusters[x]:
+                    for c2 in clusters[y]:
+                        cluster_similarity[i] = similarity_matrix[c1][c2]
+                        i += 1
+                
+                if similarity_method == 0:
+                    d = min(cluster_similarity)
+                elif similarity_method == 1:
+                    d = max(cluster_similarity)
+                elif similarity_method == 2:
+                    d = sum(cluster_similarity) / len(cluster_similarity)
+                else:
+                    raise Exception('Bad Similarity Method!')
+
+                # cluster1_points = [points[i] for i in clusters[x]]
+                # cluster2_points = [points[i] for i in clusters[y]]
+                # d = get_cluster_distrance(cluster1_points, cluster2_points, similarity_method)
                 if d < min_distance:
                     min_x = x
                     min_y = y
@@ -66,19 +94,19 @@ def getAHC(points, K, distance_method):
 #                 [3, 2]]
 
 # Cluster_K = 2
-# Cluster_Distance_Method = 0
+# Cluster_Similarity_Method = 0
 # Coordinates_Count = len(coordinates)
 
 head = [int(i) for i in input().split()] 
 Coordinates_Count = head[0]
 Cluster_K = head[1]
-Cluster_Distance_Method = head[2]
+Cluster_Similarity_Method = head[2]
 coordinates = [0 for i in range(Coordinates_Count)]
 for i in range(Coordinates_Count):
     pt = [float(i) for i in input().split()] 
     coordinates[i] = pt
 
-clusters = getAHC(coordinates, Cluster_K, Cluster_Distance_Method)
+clusters = getAHC(coordinates, Cluster_K, Cluster_Similarity_Method)
 
 output =  [0 for i in range(Coordinates_Count)]
 for cluster_id in range(Cluster_K):
